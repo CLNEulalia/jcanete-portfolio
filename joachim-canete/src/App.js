877c51eb0c2bd40603c621faa563a0ab5719771d
jcanete <joachimcanete@gmail.com>
2021-03-26 20:23:31 -0500

    router and nav bar in

--- joachim-canete/src/App.js
+++ joachim-canete/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Home from './Components/Home.jsx';
+import { Switch } from 'react-router-dom'
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Navigation/>
+      </Switch>
       <Home/>
     </div>
   );
---
+++ joachim-canete/src/Components/Navigation.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Navigation () {
+    return (
+        <div><h1>Nav Bar HERE</h1></div>
+    )
+}
