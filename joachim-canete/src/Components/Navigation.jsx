d7efaf857af01c08eab22bd58c8b77a144d9545e
jcanete <joachimcanete@gmail.com>
2021-04-07 13:18:42 -0500

    nav bar looking real good now

--- joachim-canete/src/Components/Navigation.jsx
+++ joachim-canete/src/Components/Navigation.jsx
@@ -1,35 +1,33 @@
 import './Styles/NavStyles.css';
-import { Nav } from 'react-bootstrap';
 
 export default function Navigation () {
     return (
         <>
-            <div>
-                <Nav as='ul'>
-                <Nav.Item as='li'>
-                    <Nav.Link href='/projects'>Projects</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as='li'>
-                    <Nav.Link href='/resume'>Resume</Nav.Link>
-                </Nav.Item>
-                </Nav>
-            </div>
-
-
-            
-            <div>
-                <a href='https://github.com/CLNEulalia'>
-                    <img src='https://i.imgur.com/rgrBHcg.png' alt='gitHub'/>
-                </a>
-                <a href='https://www.linkedin.com/in/joachim-canete/'>
-                    <img src='https://i.imgur.com/5hE6V3W.png' alt='linkedin'/>
-                </a>
-                <a href='mailto:joachimcanete@gmail.com'>
-                    <img src='https://i.imgur.com/oQiH9ek.png' alt='email'/>
-                </a>
-                <a href='https://twitter.com/Eulalia19486588'>
-                    <img src='https://i.imgur.com/Nfs16I2.png' alt='twitter'/>
-                </a>
+            <div className='nav-bar'>
+                <a className='nav-name' href='/'>Joachim Canete</a>
+                <div className='nav-social'>
+                    <a href='https://github.com/CLNEulalia'>
+                        <img
+                            className='social-icon'
+                            src='https://i.imgur.com/rgrBHcg.png' alt='gitHub'/>
+                    </a>
+                    <a href='https://www.linkedin.com/in/joachim-canete/'>
+                        <img
+                            className='social-icon'
+                            src='https://i.imgur.com/5hE6V3W.png' alt='linkedin'/>
+                    </a>
+                    <a href='mailto:joachimcanete@gmail.com'>
+                        <img
+                            className='social-icon'
+                            src='https://i.imgur.com/oQiH9ek.png' alt='email'/>
+                    </a>
+                    <a href='https://twitter.com/Eulalia19486588'>
+                        <img
+                            className='social-icon'
+                            src='https://i.imgur.com/Nfs16I2.png' alt='twitter'/>
+                    </a>
+                </div>
+                <a className='nav-resume' href='https://19382163-7a60-4ed3-85a9-51abf6d68295.filesusr.com/ugd/132a6b_586f328265f3434e8b4b7f442edd1664.pdf'>Resume</a>
             </div>
         </>
     )
